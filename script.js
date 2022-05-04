8b732edddec512003b2f0cf79976ef10e69d01b8
Niccolochezzo <niccolochezzo@gmail.com>
2022-05-04 16:26:54 +0400

    ...

---
+++ script.js
@@ -0,0 +1,21 @@
+// Taskk 1
+let array = [1, 6, 9, 143, 999, 65, 354, 3241, 765,]
+console.log(Math.max(...array))
+console.log(Math.min(...array))
+
+// Task 2
+let number1 = Math.pow(3,4);
+console.log(number1);
+
+let number2 = Math.pow(2, 3);
+console.log(number2);
+
+// Task 3
+
+let arrow = (a, b) => {
+    let arrFunc = a ** b;
+    return arrFunc;
+}
+
+let result = arrow(5, 3);
+console.log(result);
